7ed1fc30d1d49e40ed49dfc9bc7623e02206f78e
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-04-18 23:36:38 +0300

    booker button

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,50 +1,59 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button, FlatList } from 'react-native'
-import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import { DATA } from '../data'
-import { Post } from '../components/Post'
-import { AppHeaderIcon } from '../components/AppHeaderIcon'
-
-
+import React from "react";
+import { View, StyleSheet, Text, Button, FlatList } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { DATA } from "../data";
+import { Post } from "../components/Post";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
 
 export const MainScreen = (props) => {
-    const openPostHeandler = (post) => {
-        
-      props.navigation.navigate('Post', {postId: post.id, date: post.date} )
-
-    }
-    return (
-        <View style={styles.wrapper}>
-            <FlatList
-                data={DATA}
-                keyExtractor={post => post.id.toString()}
-                renderItem={({ item }) => <Post post={item} onOpen={openPostHeandler}/>}
-            />
-
-        </View>
-    ) 
-}
+  const openPostHeandler = (post) => {
+    props.navigation.navigate("Post", {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked,
+    });
+  };
+  return (
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({ item }) => (
+          <Post post={item} onOpen={openPostHeandler} />
+        )}
+      />
+    </View>
+  );
+};
 MainScreen.navigationOptions = {
-    headerTitle: 'Мой блог',
-    headerRight: () => (
-        <HeaderButtons
-            HeaderButtonComponent={AppHeaderIcon}>
-            <Item
-                title='Take photo'
-                iconName='ios-camera'
-                onPress={() => console.log('Pressed photo')}
-            />
-        </HeaderButtons>
-    )
-}
+  headerTitle: "Мой блог",
+  headerRight: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Take photo"
+        iconName="ios-camera"
+        onPress={() => console.log("Pressed photo")}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Toogle Drawer"
+        iconName="ios-menu"
+        onPress={() => console.log("Pressed menu")}
+      />
+    </HeaderButtons>
+  ),
+};
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    wrapper: {
-        padding: 10
-    }
-})
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  wrapper: {
+    padding: 10,
+  },
+});
