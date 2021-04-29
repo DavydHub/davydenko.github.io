7493dccab2719b7e6d81165ea12099155d848cb5
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-04-29 20:38:04 +0300

    BottomNavigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,24 +1,74 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import { Platform } from 'react-native'
-import { MainScreen } from '../screens/MainScreen'
-import { PostScreen } from '../screens/PostScreen'
-import { THEME } from '../theme'
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MainScreen } from "../screens/MainScreen";
+import { PostScreen } from "../screens/PostScreen";
+import { THEME } from "../theme";
+import { BookedScreen } from "../screens/BookedScreen";
 
-
-const PostNavigator = createStackNavigator({
+const PostNavigator = createStackNavigator(
+  {
     Main: MainScreen,
     Post: {
-        screen: PostScreen
-    }
-}, {
-    initialRouteName: 'Main',
+      screen: PostScreen,
+    },
+  },
+  {
+    initialRouteName: "Main",
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
-        },
-        headerTintColor: Platform.OS === 'IOS' ? THEME.MAIN_COLOR : '#fff',
-    }
-})
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff",
+      },
+      headerTintColor: Platform.OS === "IOS" ? THEME.MAIN_COLOR : "#fff",
+    },
+  }
+);
 
-export const AppNavigation = createAppContainer(PostNavigator)
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookedScreen,
+    Post: {
+      screen: PostScreen,
+    },
+  },
+  {
+    initialRouteName: "Booked",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff",
+      },
+      headerTintColor: Platform.OS === "IOS" ? THEME.MAIN_COLOR : "#fff",
+    },
+  }
+);
+
+const BottomNavigator = createBottomTabNavigator(
+  {
+    Post: {
+      screen: PostNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+    Booked: {
+      screen: BookedNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-star" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activetintColor: THEME.MAIN_COLOR,
+    },
+  }
+);
+
+export const AppNavigation = createAppContainer(BottomNavigator);
