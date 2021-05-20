78003026c1714628c80dbc85b7ff32b023ba3910
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-05-20 19:38:04 +0300

    Update and Remove posts from DB

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -32,8 +32,8 @@ export const PostScreen = ({ navigation }) => {
   }, [booked]);
 
   const toggleHeandler = useCallback(() => {
-    dispatch(toggleBooked(postId));
-  }, [dispatch, postId]);
+    dispatch(toggleBooked(post));
+  }, [dispatch, post]);
 
   useEffect(() => {
     navigation.setParams({ toggleHeandler });
